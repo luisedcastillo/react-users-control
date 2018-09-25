91e903e7a7412e07f644077226c909b2bb247678
Luis Castillo <luised.castillo@gmail.com>
2018-09-24 21:06:41 -0500

    Add simple about page and add grid to home page

--- src/containers/about-container.js
+++ src/containers/about-container.js
@@ -1,8 +1,35 @@
 import React from 'react';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
+import { Grid, Row, Col } from 'react-flexbox-grid';
+
 
 const AboutContainer = () => (
     /* jshint ignore:start */ // JSX is not supported
-    <div>About</div>
+    <Grid >
+        <Row center="xs">
+            <Col xs={12} md={8}>
+                <Paper className="about-container">
+                    <Grid>
+                        <Row start="xs">
+                            <Col>
+                                <Typography variant="display2" color="inherit" gutterBottom>
+                               This is about information
+                                </Typography>
+                                <Typography variant="headline" color="inherit" paragraph>
+                                Lorem ipsum dolor sit amet consectetur adipiscing elit blandit sodales class facilisis, tristique dictum etiam at volutpat tempor suspendisse nostra conubia tempus odio nascetur, vehicula convallis ligula in justo nec sapien eleifend fermentum magna. Quam dictumst nec enim potenti natoque vivamus odio montes, fringilla viverra proin non leo mollis. 
+                                </Typography>
+                                <Typography variant="title" color="inherit">
+                                Continue reading...
+                                </Typography>
+                        </Col>
+                    </Row>
+                </Grid>
+            </Paper>
+        </Col>
+    </Row>
+</Grid>
+    
     /* jshint ignore:start */ // JSX is not supported
 );
 
--- src/containers/home-container.js
+++ src/containers/home-container.js
@@ -1,19 +1,28 @@
 import React from 'react';
 import Typography from '@material-ui/core/Typography';
+import Paper from '@material-ui/core/Paper';
+import { Grid, Row, Col } from 'react-flexbox-grid';
 import './styles.css';
 
 const HomeContainer = () => {
     return(
         /* jshint ignore:start */ // JSX is not supported
-        <div>
-            <img src={require(`./../assets/images/hexaware2.jpeg`)} alt="logo" className="banner"></img>
-            <Typography variant="display1" align="center" color="textPrimary" gutterBottom>
-              Financial
-            </Typography>
-            <Typography variant="title" align="center" color="textSecondary" paragraph>
-            Hexaware Technologies Limited (NSE:HEXAWARE), is a ₹126.50b small-cap, which operates in the IT services industry based in India. Technology has become a vital component of every industry, bringing unprecedented opportunities for growth, along with challenges and competition from traditional and emerging areas. Innovations such as augmented and virtual reality, blockchain, machine learning and autonomous vehicles are paving the way for tech sector growth and branching out into new applications. However, more specifically in the IT service industry, tech analysts are forecasting a positive double-digit growth of 15.1% in the upcoming year , and a massive growth of 58.1% over the next couple of years. However this rate still came in below the growth rate of the Indian stock market as a whole. Is now the right time to pick up some shares in tech companies? Today, I will analyse the industry outlook, as well as evaluate whether Hexaware Technologies is lagging or leading its competitors in the industry.
-            </Typography>
-        </div>
+        <Grid>
+            <Row center="xs">
+                <Col xs={10}>
+                    <Paper className="home-container">
+                        <img src={require(`./../assets/images/hexaware2.jpeg`)} alt="logo" className="banner"></img>
+                        <Typography variant="display1" align="center" color="textPrimary" gutterBottom>
+                        Financial
+                        </Typography>
+                        <Typography variant="title" align="center" color="textSecondary" paragraph>
+                        Hexaware Technologies Limited (NSE:HEXAWARE), is a ₹126.50b small-cap, which operates in the IT services industry based in India. Technology has become a vital component of every industry, bringing unprecedented opportunities for growth, along with challenges and competition from traditional and emerging areas. Innovations such as augmented and virtual reality, blockchain, machine learning and autonomous vehicles are paving the way for tech sector growth and branching out into new applications. However, more specifically in the IT service industry, tech analysts are forecasting a positive double-digit growth of 15.1% in the upcoming year , and a massive growth of 58.1% over the next couple of years. However this rate still came in below the growth rate of the Indian stock market as a whole. Is now the right time to pick up some shares in tech companies? Today, I will analyse the industry outlook, as well as evaluate whether Hexaware Technologies is lagging or leading its competitors in the industry.
+                        </Typography>
+                    </Paper>
+                </Col>
+            </Row>
+        </Grid>
+
         /* jshint ignore:end */ // JSX is not supported
     );
 };
